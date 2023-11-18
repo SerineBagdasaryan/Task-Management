28dc4dd77e0e75487b3084613523d8139cf35dcf
Serine Baghdasaryan <serinebagdasaryan5@gmail.com>
2023-11-18 16:46:26 +0400

    remove unused code

--- src/common/interceptors/logging-response.interceptor.ts
+++ src/common/interceptors/logging-response.interceptor.ts
@@ -1,24 +1,3 @@
-// import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-// import { Observable } from 'rxjs';
-// import { tap } from 'rxjs/operators';
-//
-// @Injectable()
-// export class LoggingResponseInterceptor implements NestInterceptor {
-//     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-//         const request = context.switchToHttp().getRequest();
-//
-//         return next.handle().pipe(
-//             tap((data) => {
-//                 const response = context.switchToHttp().getResponse();
-//                 console.log(`[${request.method}] ${request.url} - Response Status Code: ${response.statusCode}`);
-//                 data = { data, statusCode: response.statusCode };
-//                 response.json(data);
-//             }),
-//         );
-//     }
-// }
-
-
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
--- src/modules/tasks/tasks.service.spec.ts
+++ src/modules/tasks/tasks.service.spec.ts
@@ -2,9 +2,6 @@ import {Test, TestingModule} from '@nestjs/testing';
 import {TasksService} from './tasks.service';
 import {getRepositoryToken} from "@nestjs/typeorm";
 import {TaskRepository} from "@/modules/tasks/entities/task.repository";
-import {CreateTaskDto} from "@/modules/tasks/dto/create-task.dto";
-import {Task} from "@/modules/tasks/entities/task.entity";
-import {TaskStatus} from "@/modules/tasks/enum/task-status.enum";
 
 describe('TasksService', () => {
   let taskService: TasksService;
@@ -27,70 +24,10 @@ describe('TasksService', () => {
     }).compile();
 
     taskService = module.get<TasksService>(TasksService);
-    // taskRepository = module.get<TaskRepository>(getRepositoryToken(TaskRepository));
   });
 
   it('should be defined', () => {
     expect(taskService).toBeDefined();
   });
-
-    xit('should create a task and return ResponseDataDTO<Task>', async () => {
-        const createTaskDto: CreateTaskDto = {
-            title: 'Task',
-            description: 'Task Description',
-            dueDate: new Date(),
-        };
-
-        const user = {
-            id: 1,
-            title: 'Task',
-            description: 'Task Description',
-            dueDate: new Date(),
-            status: TaskStatus.TODO,
-            userId: 1,
-        } as Task;
-
-        // Modify the mock implementation to return a resolved promise
-        const s = jest.spyOn(mockTaskRepository, 'save').mockResolvedValue(user);
-        console.log(11111, s);
-        // act
-        const result = await taskService.createTask(createTaskDto, 1);
-
-        // assert
-        expect(mockTaskRepository.save).toBeCalled();
-        expect(mockTaskRepository.save).toBeCalledWith(createTaskDto);
-
-        expect(result).toEqual(user);
-    });
-
-    // it('should create a task and return ResponseDataDTO<Task>', async () => {
-  //     const createTaskDto: CreateTaskDto = {
-  //       title: 'Task',
-  //       description: 'Task Description',
-  //       dueDate: new Date(),
-  //     };
-  //
-  //     const user = {
-  //         id: 1,
-  //         title: 'Task',
-  //         description: 'Task Description',
-  //         dueDate: new Date(),
-  //         status: TaskStatus.TODO,
-  //         userId: 1
-  //     } as Task;
-  //
-  //     jest.spyOn(mockTaskRepository, 'save').mockReturnValue(user);
-  //
-  //     // act
-  //     const result = await taskService.create(createTaskDto);
-  //
-  //     // assert
-  //     expect(mockTaskRepository.save).toBeCalled();
-  //     expect(mockTaskRepository.save).toBeCalledWith(createTaskDto);
-  //
-  //     expect(result).toEqual(user);
-  //
-  //
-  //   });
 
 });
