4bac4693681c69e381824981fe24029b76517709
Serine Baghdasaryan <serinebagdasaryan5@gmail.com>
2023-11-30 14:41:45 +0400

    remove unused code

--- src/common/decorators/item-response-type.decorator.ts
+++ src/common/decorators/item-response-type.decorator.ts
@@ -1,54 +1,12 @@
-// import {Type} from "@nestjs/common";
-// import {ApiResponse, getSchemaPath} from "@nestjs/swagger";
-//
-// export function ItemResponseTypeDecorator<T>(
-//     classRef: Type<T>,
-//     statusCode: number = 200,
-//     statusName: string = 'OK',
-// ) {
-//     return ApiResponse({
-//         status: statusCode,
-//         description: statusName,
-//         type: classRef,
-//         schema: {
-//             properties: {
-//                 data: {
-//                     type: 'array',
-//                     items: { $ref: getSchemaPath(classRef) },
-//                 },
-//             },
-//         },
-//     });
-// }
-// import {applyDecorators, Type} from '@nestjs/common';
-// import { ApiResponse, getSchemaPath } from '@nestjs/swagger';
-//
-// export const ItemResponseTypeDecorator =<T> (
-//     model: Type<T>,
-//     statusCode: number = 200,
-//     statusName: string = 'OK',
-// ) => {
-//     return applyDecorators(
-//         ApiResponse({
-//             status: statusCode,
-//             description: statusName,
-//             schema: Array.isArray(model)
-//                 ? {
-//                     type: 'array',
-//                     items: { $ref: getSchemaPath(model[0]) },
-//                 }
-//                 : { $ref: getSchemaPath(model) },
-//         }),
-//     );
-// };
 import { applyDecorators, Type } from '@nestjs/common';
 import { ApiResponse, getSchemaPath } from '@nestjs/swagger';
 import { HttpStatus } from '@nestjs/common';
+import { STATUS_CODES } from "http";
 
 export const ItemResponseTypeDecorator = <T>(
-    model:  Type<T> | Type<T>[],
+    model: Type<T> | Type<T>[],
     statusCode: number = HttpStatus.OK,
-    statusName: string = 'OK',
+    statusName: string = STATUS_CODES[HttpStatus.OK],
 ) => {
     return applyDecorators(
         ApiResponse({
@@ -59,9 +17,9 @@ export const ItemResponseTypeDecorator = <T>(
                     schema: Array.isArray(model)
                         ? {
                             type: 'array',
-                            items: { $ref: getSchemaPath(model[0]) },
+                            items: {$ref: getSchemaPath(model[0])},
                         }
-                        : { $ref: getSchemaPath(model) },
+                        : {$ref: getSchemaPath(model)},
                 },
             },
         }),
